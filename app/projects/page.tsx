b149246d7d4b10d2e40228257c7127b7180d8730
bibash21-creator <bibashworkmail21@gmail.com>
2025-08-14 20:35:06 +0545

    Added Projects Routing

---
+++ app/projects/page.tsx
@@ -0,0 +1,15 @@
+import  Navbar from "@/app/navbar/Navbar"
+
+import Project from "@/app/projects/Project"
+
+
+export default function Page(){
+    return(
+        <>
+        <Navbar />
+
+        <Project />
+        
+        </>
+    )
+}
