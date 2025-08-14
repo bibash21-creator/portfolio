df9ffb91b4a33934bb054229fe6a6d574f825b7a
bibash21-creator <bibashworkmail21@gmail.com>
2025-08-14 20:36:39 +0545

    Added Skills Routing as well

---
+++ app/skills/page.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/app/navbar/Navbar"
+
+import Skills from "@/app/skills/Skills"
+
+
+
+export default function Page(){
+    return(
+        <>
+        <Navbar />
+
+        <Skills />
+        </>
+    )
+}
