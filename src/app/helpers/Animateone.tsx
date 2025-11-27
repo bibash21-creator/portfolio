947f6683f9de78d85cb39a1d25617a3e2a66fa75
bibash21-creator <bibashworkmail21@gmail.com>
2025-11-27 20:02:26 +0545

    Added orbiting sphere

--- src/app/helpers/Animateone.tsx
+++ src/app/helpers/Animateone.tsx
@@ -3,10 +3,8 @@
 
 import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls, Stars } from '@react-three/drei'
-import { useRef } from 'react'
+import { useRef, memo } from 'react'
 import * as THREE from 'three'
-
-
 
 export default function OrbitingSkillsScene() {
   return (
@@ -14,38 +12,49 @@ export default function OrbitingSkillsScene() {
       <Canvas camera={{ position: [0, 0, 10], fov: 45 }}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[5, 5, 5]} />
-        <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade />
+
+        <Stars radius={100} depth={50} count={5000} factor={4} fade />
+
         <OrbitControls enableZoom={true} />
+
         <OrbitingSkills />
       </Canvas>
     </div>
   )
 }
 
-function OrbitingSkills() {
+// -------------------------------
+// OPTIMIZED ORBITING SKILLS GROUP
+// -------------------------------
+
+const OrbitingSkills = memo(function OrbitingSkills() {
   const groupRef = useRef<THREE.Group>(null!)
 
+  // rotating animation
   useFrame(() => {
-    if (groupRef.current) {
-      groupRef.current.rotation.y += 0.005 // slow orbit
-    }
+    groupRef.current.rotation.y += 0.005
   })
+
+  // typed positions to avoid TypeScript errors
+  const positions: [number, number, number][] = [
+    [3, 0, 0],
+    [-3, 0, 0],
+    [0, 3, 0],
+    [0, -3, 0],
+    [2.1, 2.1, 0],
+    [-2.1, -2.1, 0],
+  ]
 
   return (
     <group ref={groupRef}>
+      {/* Central Sphere */}
       <mesh>
         <sphereGeometry args={[1, 32, 32]} />
         <meshStandardMaterial color="#805AD5" />
       </mesh>
 
-      {[
-        [3, 0, 0],
-        [-3, 0, 0],
-        [0, 3, 0],
-        [0, -3, 0],
-        [2.1, 2.1, 0],
-        [-2.1, -2.1, 0]
-      ].map((pos, i) => (
+      {/* Orbiting Spheres */}
+      {positions.map((pos, i) => (
         <mesh key={i} position={pos}>
           <sphereGeometry args={[0.3, 16, 16]} />
           <meshStandardMaterial color="#F6E05E" />
@@ -53,4 +62,4 @@ function OrbitingSkills() {
       ))}
     </group>
   )
-}
+})
